8ad49dbc831d21d442ad5fdaf15e055e4f97712c
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2021-10-05 08:04:20 +0100

    :bookmark: jekyll-dash 1.4.0

--- .github/actions/gemspec-fetch/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SPEC_DATA=$(parse-gemspec-cli $INPUT_SPECFILE)
-
-echo "::set-output name=name::$(echo $SPEC_DATA | jq -r '.name')"
-echo "::set-output name=description::$(echo $SPEC_DATA | jq -r '.description')"
-echo "::set-output name=summary::$(echo $SPEC_DATA | jq -r '.summary')"
-echo "::set-output name=version::$(echo $SPEC_DATA | jq -r '.version')"
-echo "::set-output name=homepage::$(echo $SPEC_DATA | jq -r '.homepage')"
